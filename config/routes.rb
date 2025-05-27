259cccfad29a8a7d7ff5742488a5c29054233857
manuelle augereau <manuelleaugereau@gmail.com>
2025-05-27 12:25:42 +0200

    add task action show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'index',        to: 'tasks#index'
+  get 'task/:id',     to: 'tasks#show', as: :task
 end

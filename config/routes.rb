75a7092c94ed4b1af3b8a7246f6d34bbe6dac1ac
manuelle augereau <manuelleaugereau@gmail.com>
2025-05-27 14:58:56 +0200

    add task action edit/update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'index',        to: 'tasks#index'
-  get 'task/:id',     to: 'tasks#show', as: :task
+  get 'index',          to: 'tasks#index'
+  get 'tasks/new',      to: 'tasks#new'
+  post 'tasks',         to: 'tasks#create'
+  get 'tasks/:id',      to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id',    to: 'tasks#update'
 end
